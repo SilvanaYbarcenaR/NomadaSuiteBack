171bfc8fc88396b0322105e4a1991bf2997b7846
anamang <anitaeatjobs@gmail.com>
2023-10-27 15:18:29 +0200

    filter bedroom ok

--- src/controllers/filters/filteredByBedrooms.js
+++ src/controllers/filters/filteredByBedrooms.js
@@ -2,37 +2,37 @@ const Accommodation = require("../../models/Accommodation");
 
 const filteredByBedrooms = async (req, res) => {
   const bedroomsQuantity = req.body.quantity;
-
+  console.log(bedroomsQuantity);
   try {
     const filteredBedrooms = await Accommodation.find().populate("idServices");
+    console.log(filteredBedrooms.length);
+    const filtered = filteredBedrooms
+      .map((accommodation) => {
+        const idServices = accommodation.idServices.filter((service) => {
+          return (
+            service.name === "Bedroom" && service.quantity === bedroomsQuantity
+          );
+        });
 
-    return res.json(filteredBedrooms);
+        if (idServices.length > 0) {
+          accommodation.idServices = idServices;
+          return accommodation;
+        }
+
+        return null;
+      })
+      .filter((accommodation) => accommodation !== null);
+
+    if (filtered.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No matching accommodations found" });
+    }
+
+    return res.json(filtered);
   } catch (error) {
     return res.status(500).json({ error: "Error processing request" });
   }
 };
 
 module.exports = filteredByBedrooms;
-
-// const filteredByBedrooms = async (req, res) => {
-//   const bedroomsQuantity = req.body.quantity;
-
-//   try {
-//     const accommodations = await Accommodation.find().populate("idServices");
-
-//     // Filtrar las habitaciones que cumplan con los criterios
-//     const filteredAccommodations = accommodations.filter((accommodation) => {
-//       const bedrooms = accommodation.idServices.filter((service) => {
-//         return (
-//           service.name === "Bedroom" && service.quantity >= bedroomsQuantity
-//         );
-//       });
-
-//       return bedrooms.length > 0;
-//     });
-
-//     return res.json(filteredAccommodations);
-//   } catch (error) {
-//     return res.status(500).json({ error: "Error processing request" });
-//   }
-// };
