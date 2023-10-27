828d3a894f304d44f32aa101142f84aa962dac9b
JulianaCelis <juliana0celis@email.com>
2023-10-27 16:40:44 -0500

    crud users funcional

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const server = express()
+const server = express();
 const router = require("./routes");
+const morgan = require('morgan');
 
-server.use(express.json())
+server.use(morgan('dev'));
+
+server.use(express.json());
 
 server.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*'); 
@@ -10,7 +13,7 @@ server.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-})
+});
 
 server.use(router);
 
