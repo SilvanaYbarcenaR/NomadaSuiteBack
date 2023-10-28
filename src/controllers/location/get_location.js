a38b6348bb8700f6b42f9afdace9cf642cdf4aa6
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-28 17:39:33 -0300

    get_location funcionando

--- src/controllers/location/get_location.js
+++ src/controllers/location/get_location.js
@@ -9,8 +9,6 @@ const getLocation = async (req, res) => {
         return res.json(allLocations);
 
 
-        res.json(location);
-
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error interno del servidor' });
