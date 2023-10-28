1b9e40376df90adb949e1e50ed9a0c55348d9b88
dobarrofranco <mr.francodobarro@gmail.com>
2023-10-28 17:33:08 -0300

    get_location funcionando

--- src/controllers/location/get_location.js
+++ src/controllers/location/get_location.js
@@ -1,32 +1,13 @@
 const LocationAccommodation = require('../../models/LocationAccommodation')
 
 const getLocation = async (req, res) => {
-    
-    const { name } = req.params;
 
     try {
-        
-        if (!name) {
-        
-            const allLocations = await LocationAccommodation.find();
-    
-            return res.json(allLocations);
-        };
 
-        const location = await LocationAccommodation.find({
-            $or: [
-                {
-                    city: {
-                        $regex: new RegExp(name, 'i')
-                    }
-                },
-                {
-                    country: {
-                        $regex: new RegExp(name, 'i')
-                    }
-                }
-            ]
-        });
+        const allLocations = await LocationAccommodation.find();
+
+        return res.json(allLocations);
+
 
         res.json(location);
 
