8b74f4177bacf0a6319ca450061ff3a2d2f64bfa
JulianaCelis <juliana0celis@email.com>
2023-11-04 08:43:52 -0500

    webhook event

--- src/routes/checkout_stripe_router.js
+++ src/routes/checkout_stripe_router.js
@@ -12,16 +12,17 @@ checkoutStripeRouter.use(bodyParser.raw({ type: 'application/json' }));
 
 
 checkoutStripeRouter.post('/webhook', async (req, res) => {
-    const sig = req.headers['stripe-signature'];
-    try {
-      const event = stripe.Webhooks.constructEventFromPayload(req.body, sig, endpointSecret);
-      console.log('Evento:', event);
-      res.json({ received: true });
-    } catch (err) {
-      console.error('Error de webhook:', err);
-      res.status(400).send(`Webhook Error: ${err.message}`);
-    }
-  });
+  const sig = req.headers['stripe-signature'];
+
+  try {
+    const event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
+    console.log('Evento:', event);
+    res.json({ received: true });
+  } catch (err) {
+    console.error('Error de webhook:', err);
+    res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+});
 
 
 module.exports = checkoutStripeRouter;
