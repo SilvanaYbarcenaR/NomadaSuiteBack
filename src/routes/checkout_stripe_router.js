66cd06d50b72c7daa2693835fb8f3532ad4656b1
JulianaCelis <juliana0celis@email.com>
2023-11-09 17:40:42 -0500

    webhook2

--- src/routes/checkout_stripe_router.js
+++ src/routes/checkout_stripe_router.js
@@ -13,26 +13,25 @@ checkoutStripeRouter.use(express.json());
 checkoutStripeRouter.post('/webhook', express.json({ type: 'application/json' }), (request, response) => {
   const event = request.body;
 
-  // Handle the event based on its type
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntent = event.data.object;
-      // Handle a successful payment intent
-      // handlePaymentIntentSucceeded(paymentIntent);
-      break;
-    case 'payment_method.attached':
-      const paymentMethod = event.data.object;
-      // Handle the successful attachment of a PaymentMethod
-      // handlePaymentMethodAttached(paymentMethod);
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
+  // Log the event information
+  console.log('Información del evento recibido:');
+  console.log(event);
+
+  // Handle the event if it's a checkout.session.completed
+  if (event.type === 'checkout.session.completed') {
+    const checkoutSession = event.data.object;
+    // Handle a completed checkout session
+    // handleCompletedCheckoutSession(checkoutSession);
+    console.log('Checkout Session ID:', checkoutSession.id);
+    // Aquí puedes realizar las acciones necesarias para un checkout completado
+  } else {
+    console.log(`Unhandled event type ${event.type}`);
   }
 
   // Respond to acknowledge receipt of the event
   response.json({ received: true });
 });
+
 
 
 module.exports = checkoutStripeRouter;
