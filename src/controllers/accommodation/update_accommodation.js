c0ea4e49b476241998fc5265620fbd78a62ea809
JulianaCelis <juliana0celis@email.com>
2023-11-16 14:01:57 -0500

    put accommodattion services

--- src/controllers/accommodation/update_accommodation.js
+++ src/controllers/accommodation/update_accommodation.js
@@ -1,4 +1,5 @@
 const Accommodation = require("../../models/Accommodation");
+const Services = require('../../models/Services');
 
 const updateAccommodation = async (req, res) => {
   const id = req.params.id;
@@ -9,23 +10,35 @@ const updateAccommodation = async (req, res) => {
       return res.status(404).json({ error: "Accommodation not found" });
     }
 
-    const updatedAccommodationData = req.body;
+    // Crear un objeto de actualización dinámico
+    const updateFields = {};
+    
+    // Verificar si se proporciona el campo 'services' en el cuerpo de la solicitud
+    if (req.body.services) {
+      // Obtener los datos de servicios usando los IDs proporcionados en 'services'
+      const selectedServices = req.body.services;
+      const servicesData = await Services.find({ _id: { $in: selectedServices } });
 
-    // Verificar si se proporcionaron nuevas imágenes
-    if (req.imageURLs) {
-      const newPhotos = Array.isArray(req.imageURLs) ? req.imageURLs : [req.imageURLs];
-      updatedAccommodationData.photos = newPhotos;
+      // Asignar los valores a 'idServices' en el modelo 'Accommodation'
+      updateFields.idServices = servicesData;
+    }
+
+    // Verificar si se proporciona el campo 'imageURLs' en el cuerpo de la solicitud
+    if (req.body.imageURLs) {
+      const newPhotos = Array.isArray(req.body.imageURLs) ? req.body.imageURLs : [req.body.imageURLs];
+      updateFields.photos = newPhotos;
     }
 
     const updatedAccommodation = await Accommodation.findOneAndUpdate(
       { _id: id },
-      { $inc: { __v: 1 }, $set: updatedAccommodationData },
+      { $inc: { __v: 1 }, $set: updateFields },
       { new: true, useFindAndModify: false }
     );
 
     if (!updatedAccommodation) {
       return res.status(404).json({ error: "Accommodation not found" });
     }
+
     res.status(200).json(updatedAccommodation);
   } catch (error) {
     res.status(400).json({ error: error.message });
