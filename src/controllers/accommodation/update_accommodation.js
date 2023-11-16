0378513ff4afde96eb5839d8c550d62541b2531e
JulianaCelis <juliana0celis@email.com>
2023-11-16 09:11:13 -0500

    accommodation update con cloudinary

--- src/controllers/accommodation/update_accommodation.js
+++ src/controllers/accommodation/update_accommodation.js
@@ -9,9 +9,17 @@ const updateAccommodation = async (req, res) => {
       return res.status(404).json({ error: "Accommodation not found" });
     }
 
+    const updatedAccommodationData = req.body;
+
+    // Verificar si se proporcionaron nuevas imágenes
+    if (req.imageURLs) {
+      const newPhotos = Array.isArray(req.imageURLs) ? req.imageURLs : [req.imageURLs];
+      updatedAccommodationData.photos = newPhotos;
+    }
+
     const updatedAccommodation = await Accommodation.findOneAndUpdate(
       { _id: id },
-      { $inc: { __v: 1 }, $set: req.body }, 
+      { $inc: { __v: 1 }, $set: updatedAccommodationData },
       { new: true, useFindAndModify: false }
     );
 
