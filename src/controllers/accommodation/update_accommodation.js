f2c573abd6c54da289102495595167653bf03826
JulianaCelis <juliana0celis@email.com>
2023-11-16 14:13:39 -0500

    update funcional 100%

--- src/controllers/accommodation/update_accommodation.js
+++ src/controllers/accommodation/update_accommodation.js
@@ -10,21 +10,17 @@ const updateAccommodation = async (req, res) => {
       return res.status(404).json({ error: "Accommodation not found" });
     }
 
-    const updateFields = {};
+    const updateFields = req.body;
 
+    // Si 'services' está presente en la solicitud, procesa y asigna los servicios
     if (req.body.services) {
-
       const selectedServices = req.body.services;
       const servicesData = await Services.find({ _id: { $in: selectedServices } });
 
       updateFields.idServices = servicesData;
     }
 
-    if (req.body.imageURLs) {
-      const newPhotos = Array.isArray(req.body.imageURLs) ? req.body.imageURLs : [req.body.imageURLs];
-      updateFields.photos = newPhotos;
-    }
-
+    // Realiza la actualización de todos los campos en 'updateFields'
     const updatedAccommodation = await Accommodation.findOneAndUpdate(
       { _id: id },
       { $inc: { __v: 1 }, $set: updateFields },
